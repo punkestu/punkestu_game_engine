1305d86059636ee0a75d1e311690cbf970440cec
punkestu <pangestubima89@gmail.com>
2021-04-25 09:34:47 +0700

    menambahkan class textureM untuk texture manager

--- src/texture.h
+++ src/texture.h
@@ -1,4 +1,17 @@
 #ifndef TEXTURE___H
 #define TEXTURE___H
 
+#include "core.h"
+
+class textureM{
+    private:
+
+    public:
+        void reg(std::string id, SDL_Texture* texture);
+        void create(std::string id, std::string path);
+        SDL_Texture* get(std::string id);
+        void del(std::string);
+        void clear();
+};
+
 #endif
