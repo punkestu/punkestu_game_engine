f0a0e08ae77d05d94a997d159c62ff0989a2574a
punkestu <pangestubima89@gmail.com>
2021-04-25 10:45:46 +0700

    menambahkan class spriteM sebagai sprite manager

--- src/sprite.h
+++ src/sprite.h
@@ -3,11 +3,31 @@
 
 #include "texture.h"
 
-struct sprite{
+struct renTex{//texture for rendering
     std::string texture;
+    SDL_Rect source;
+};
+
+struct sprite{
+    renTex texture;
     vect2d position;
     vect2d scale;
     SDL_Color color;
+};
+
+typedef std::map<std::string, sprite> sprMap;
+class spriteM{
+    private:
+        sprMap _sprites;
+    public:
+        void reg(std::string id, sprite* spr);
+        void create(std::string id);
+
+        sprite get(std::string id);
+        sprMap getAll();
+
+        void del(std::string id);
+        void clear();
 };
 
 #endif
