054d1df34b1945e57151507d3208350de42f8cc6
punkestu <pangestubima89@gmail.com>
2021-04-25 11:16:40 +0700

    minor

--- src/core.cpp
+++ src/core.cpp
@@ -1,9 +1,9 @@
 #include "core.h"
 
 void warning(std::string reason, std::string warn){
-    printf("warning[%s]: %s", reason, warn);
+    printf("warning[%s]: %s", reason.c_str(), warn.c_str());
 }
 
 void error(std::string reason, std::string err){
-    printf("error[%s]: %s", reason, err);
+    printf("error[%s]: %s", reason.c_str(), err.c_str());
 }
