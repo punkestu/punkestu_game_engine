73d226d61120ad63bf8ccfa2c2357456122f2919
punkestu <pangestubima89@gmail.com>
2021-04-25 09:57:55 +0700

    menambahkan definisi untuk fungsi dalam class textureM

---
+++ src/texture.cpp
@@ -0,0 +1,60 @@
+#include "texture.h"
+
+void textureM::reg(std::string id, SDL_Texture* texture){
+    if(texture != NULL){
+        if(_textures.find(id) == _textures.end()){
+            _textures[id] = texture;
+        }else{
+            printf("warning[texture]: id has been used");
+        }
+    }else{
+        printf("warning[texture]: texture not valid");
+    }
+}
+
+void textureM::create(SDL_Renderer* renderer, std::string id, std::string path){
+    if(_textures.find(id) == _textures.end()){
+        SDL_Surface* surf = IMG_Load(path.c_str());
+        if(surf != NULL){
+            SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, surf);
+            if(tex != NULL){
+                _textures[id] = tex;
+            }else{
+                printf("warning[texture]: texture not valid");
+            }
+        }else{
+            printf("warning[texture]: path not valid");
+        }
+    }else{
+        printf("warning[texture]: id has been used");
+    }
+}
+
+texMap& textureM::getAll(){
+    return _textures;
+}
+
+SDL_Texture* textureM::get(std::string id){
+    if(_textures.find(id) != _textures.end()){
+        return _textures[id];
+    }else{
+        printf("warning[texture]: texture not found");
+        return NULL;
+    }
+}
+
+void textureM::del(std::string id){
+    if(_textures.find(id) != _textures.end()){
+        SDL_DestroyTexture(_textures[id]);
+        _textures.erase(id);
+    }else{
+        printf("warning[texture]: texture not found");
+    }
+}
+
+void textureM::clear(){
+    for(texMap::iterator it = _textures.begin(); it != _textures.end(); it++){
+        SDL_DestroyTexture(it->second);
+    }
+    _textures.clear();
+}
--- src/texture.h
+++ src/texture.h
@@ -3,13 +3,17 @@
 
 #include "core.h"
 
+typedef std::map<std::string, SDL_Texture*> texMap;
 class textureM{
     private:
-
+        texMap _textures;
     public:
         void reg(std::string id, SDL_Texture* texture);
-        void create(std::string id, std::string path);
+        void create(SDL_Renderer* renderer, std::string id, std::string path);
+        
+        texMap& getAll();
         SDL_Texture* get(std::string id);
+        
         void del(std::string);
         void clear();
 };
