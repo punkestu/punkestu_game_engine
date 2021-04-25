6e28b9d772184a620863cd5d13fd6ad4dc8ace7e
Moch Bima Pangestu <39290636+punkestu@users.noreply.github.com>
2021-04-25 12:56:06 +0700

    comment

--- src/core.h
+++ src/core.h
@@ -9,27 +9,31 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
+//to warn (reason is where the warning come from, warn is the warning message)
 void warning(std::string reason, std::string warn);
+//to error (reason is where the error come from, err is the error message)
 void error(std::string reason, std::string err);
 
+//2d point
 struct vect2d
 {
     int x,y;
 };
 
+//timer
 class timer{
     private:
-        uint timeElp;
-        uint frmTm;
-        uint litTm;
-        uint getDelta(uint& base);
-        void reset(uint& base);
+        uint timeElp;//elapsed time from init
+        uint frmTm;//frame limit time
+        uint litTm;//literal time limit
+        uint getDelta(uint& base);//get time elapsed from reset
+        void reset(uint& base);//reset specific timer variable
     public:
-        void init();
-        void restart();
-        bool frmLim(uint fps);
-        bool timLim(uint tm);
-        uint getTimeElp();
+        void init();//init timer
+        void restart();//restart all of timer variable
+        bool frmLim(uint fps);//limit frame
+        bool timLim(uint tm);//limit time
+        uint getTimeElp();//get elapsed time
 };
 
 #endif
